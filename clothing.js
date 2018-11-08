33f55ac421350559440f00eaa8a4e0f41c319c9d
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-11-07 19:17:33 -0600

    added complements function to determine if two items of clothiing complement eachother

--- clothing.js
+++ clothing.js
@@ -17,8 +17,26 @@ module.exports = class Clothing {
     get lighting() {
 	return this.hsl[2];
     }
+
+    // Usage: a.matches(b)
+    // Returns: floating point in [0, 1]
+    //          1 -> b is a perfect complement to a
+    //          0 -> b has exactly the same hue as a
+    complements(clothing) {
+	var a_hue = this.hue;
+	var b_hue = clothing.hue;
+
+	// The perfect complement to a_hue
+	var ideal_hue = (a_hue + 180) % 360;
+
+	// Calculate how far away b_hue is from being a_hue's ideal complement
+	var diff = Math.abs(ideal_hue - b_hue);
+
+	return 1 - diff / 180;
+    }
 };
 
 // Put this at the top of your file to use this class
 // var Clothing = require ("./clothing.js");
+// npm i rgb-to-hsl
 
